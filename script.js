e2fcbb03bf92cd627b2f1d8602b84a10e5427af4
Martin Atanasov <134212613+Martin-Atanasov123@users.noreply.github.com>
2025-01-26 18:26:40 +0200

    feat: add navigation menu and filter options to enhance user experience

--- script.js
+++ script.js
@@ -44,3 +44,14 @@ function filterAndSearchDishes(searchValue, filter) {
 function toggleDarkMode() {
     document.body.classList.toggle('dark-mode');
 }
+
+
+
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
